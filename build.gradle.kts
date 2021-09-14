ee07c7f4a08604f827e9593bfe4eb2f923524209
Christian Dräger <christian.draeger1@gmail.com>
2021-09-14 13:17:20 +0200

    build: add publishing to maven central and bump gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,23 @@
+@file:Suppress("LocalVariableName")
+
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm")
+    `java-library`
+    `maven-publish`
+    signing
+    id("io.github.gradle-nexus.publish-plugin")
+    id("org.jetbrains.dokka")
+    id("com.github.ben-manes.versions")
+    id("com.adarshr.test-logger")
+    jacoco
 }
 
 group = "io.github.christian-draeger"
-version = "0.1.0"
+val release_version: String by project
+version = release_version
 
 repositories {
     mavenCentral()
@@ -23,13 +35,84 @@ tasks {
     withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
     }
-    test {
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+    withType<JacocoReport> {
+        reports {
+            xml.isEnabled = true
+        }
+    }
+    withType<Test> {
         useJUnitPlatform()
+        testLogging {
+            events(
+                TestLogEvent.PASSED,
+                TestLogEvent.SKIPPED,
+                TestLogEvent.FAILED
+            )
+        }
     }
 }
 
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(8))
+        vendor.set(JvmVendorSpec.ADOPTOPENJDK)
+    }
+    withJavadocJar()
+    withSourcesJar()
+}
+
+testlogger {
+    setTheme("mocha-parallel")
+    slowThreshold = 1000
+}
+
+jacoco {
+    toolVersion = "0.8.6"
+}
+
+nexusPublishing {
+    repositories {
+        sonatype()
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = rootProject.name
+            from(components["java"])
+            pom {
+                name.set("k6-kotlin")
+                description.set("run k6 load and performance testing scenarios from your JVM project on an out-of-the box but configurable k6 infra spin-up by the use of Docker.")
+                url.set("https://github.com/christian-draeger/${rootProject.name}")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("christian-draeger")
+                        name.set("Christian Dräger")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/christian-draeger/${rootProject.name}.git")
+                    developerConnection.set("scm:git:ssh://github.com:christian-draeger/${rootProject.name}.git")
+                    url.set("https://github.com/christian-draeger/${rootProject.name}/tree/main")
+                }
+            }
+        }
+    }
+    signing {
+        sign(publishing.publications["mavenJava"])
+        val signingKeyId: String? by project
+        val signingKey: String? by project
+        val signingPassword: String? by project
+        useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,22 @@
+@file:Suppress("LocalVariableName")
+
 rootProject.name = "k6-kotlin"
+
+pluginManagement {
+    val kotlin_version: String by settings
+    repositories {
+        gradlePluginPortal()
+    }
+    plugins {
+        kotlin("jvm") version kotlin_version
+        id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+        id("org.jetbrains.dokka") version "1.4.30"
+        id("com.adarshr.test-logger") version "3.0.0"
+        jacoco
+        `java-library`
+        `maven-publish`
+        signing
+        id("com.github.ben-manes.versions") version "0.38.0"
+    }
+}
+
